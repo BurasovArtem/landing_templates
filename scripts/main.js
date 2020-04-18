765da077644826957c7f3b403c8790f13585fd34
RickSunchez <nikitalternative95@gmail.com>
2020-04-18 16:35:29 +0500

    init

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	switchHeader()
+
+	//thx to: https://qna.habr.com/q/88507
+	$("body").on('click', '[href*="#"]', function(e){
+		var fixed_offset = $("header").height();
+		$('html,body')
+			.stop()
+			.animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 500);
+
+		e.preventDefault();
+	});
+
+	//thx to: https://webkab.ru/prokrutka-stranicy-jquery-uznat-polozhenie/
+	//thx to: https://qna.habr.com/q/215411
+	$(window).on("scroll", function() {
+    	switchHeader();
+    });
+})
+
+function switchHeader() {
+	if ($(window).scrollTop() > 50) 
+		$('header').addClass('green');
+	else 
+		$('header').removeClass('green');
+}
